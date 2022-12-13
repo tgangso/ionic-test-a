e8471ce7f37478d8f3f8af41d244b9f03d931c08
Thomas Gangsøy <tgangso@gmail.com>
2022-12-13 15:28:35 +0100

    example search working fine with no flicker

--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgAisModule } from 'angular-instantsearch';
 
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { FolderPageRoutingModule } from './folder-routing.module';
-
 import { FolderPage } from './folder.page';
 
 @NgModule({
@@ -13,8 +13,9 @@ import { FolderPage } from './folder.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    FolderPageRoutingModule,
+    NgAisModule.forRoot(),
   ],
-  declarations: [FolderPage]
+  declarations: [FolderPage],
 })
 export class FolderPageModule {}
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,12 @@
+import algoliasearch from 'algoliasearch/lite';
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+const searchClient = algoliasearch(
+  'B1G2GM9NG0',
+  'aadef574be1f9252bb48d4ea09b5cfe5'
+);
 
 @Component({
   selector: 'app-folder',
@@ -7,12 +14,26 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
-  public folder: string;
+  public folder!: string;
+  public canShowMoreButton = true;
+  public results: any;
+
+  config = {
+    indexName: 'demo_ecommerce',
+    searchClient
+  };
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
   }
 
+  public test(p1: any, p2: any){
+    console.log('test');
+  }
+
+  public viewItem(p1: any){
+    console.log('view');
+  }
 }
